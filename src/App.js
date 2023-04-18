2b76b42e6019040a7d4e69010491c5854591b8ee
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2023-04-18 18:01:12 +0300

    Use some demo mui components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Button, Typography } from "@mui/material";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button variant="contained" color="secondary">
+        Settings
+      </Button>
+      <Button variant="outlined" color="success">
+        Add new post
+      </Button>
+      <Typography variant="h1" component="p">
+        It uses h1 style, but it's a p tag
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "skyblue",
+          color: "#888",
+          m: 5,
+          "&:hover": {
+            backgroundColor: "lightblue",
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white",
+          },
+        }}
+        disabled
+      >
+        My unique button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
