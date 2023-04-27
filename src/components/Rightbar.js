b6670019d1f03676da7be7ccf11f0e701afd6272
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2023-04-27 18:37:39 +0300

    Add rightbar

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,15 +1,147 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const Rightbar = () => {
   return (
-    <Box
-      bgcolor="lightcoral"
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Rightbar
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={4}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://mui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://mui.com/static/images/avatar/4.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mb={2} mt={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} gap={5}>
+          <ImageListItem key={1}>
+            <img
+              src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=164&h=164&fit=crop&auto=format&dpr=2"
+              alt="Breakfast"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem key={2}>
+            <img
+              src="https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=164&h=164&fit=crop&auto=format&dpr=2"
+              alt="Burger"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem key={3}>
+            <img
+              src="https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=164&h=164&fit=crop&auto=format&dpr=2"
+              alt="Camera"
+              loading="lazy"
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mb={2} mt={2}>
+          Latest Conversations
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          disablePadding
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="https://mui.com/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
